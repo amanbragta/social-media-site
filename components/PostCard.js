26e6c645b84ecbd2a5bc0d6546b4ed6fbe7f9074
amanbragta <amanbragta@gmail.com>
2025-03-11 18:10:38 +0530

    added functionality to remove comments

--- components/PostCard.js
+++ components/PostCard.js
@@ -52,6 +52,7 @@ export default function PostCard({
     supabase
       .from("posts")
       .select("*,profiles(*)")
+      .order("created_at", { ascending: false })
       .eq("parent", id)
       .then((result) => setComments(result.data));
   }
@@ -75,6 +76,17 @@ export default function PostCard({
       })
       .then(() => fetchLikes());
   }
+
+  function deleteComment(post_id) {
+    supabase
+      .from("posts")
+      .delete()
+      .eq("id", post_id)
+      .then(() => {
+        fetchComments();
+      });
+  }
+
   function toggleSaved() {
     if (isSaved) {
       supabase.auth.getSession().then((result) => {
@@ -298,23 +310,6 @@ export default function PostCard({
           </svg>
           {comments?.length}
         </button>
-        <button className="flex gap-2 items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-            />
-          </svg>
-          4
-        </button>
       </div>
       <div className="flex gap-2 mt-4">
         <div>
@@ -353,7 +348,7 @@ export default function PostCard({
           <div key={comment.id} className="flex items-center gap-2 mt-2">
             <Avatar url={comment.profiles.avatar} />
             <div className="bg-gray-200 py-2 px-4 rounded-3xl">
-              <div className="">
+              <div className="inline-flex items-center">
                 <Link href={"/profile/" + comment.profiles.id}>
                   <span className="hover:underline font-semibold mr-2">
                     {comment.profiles.name}
@@ -365,6 +360,25 @@ export default function PostCard({
                     date={new Date(comment.created_at).getTime()}
                   />
                 </span>
+                {profile?.id === comment.profiles.id && (
+                  <span className="ml-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="red"
+                      className="size-4"
+                      onClick={() => deleteComment(comment.id)}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </span>
+                )}
               </div>
               <p className="text-sm leading-4">{comment.content}</p>
             </div>
